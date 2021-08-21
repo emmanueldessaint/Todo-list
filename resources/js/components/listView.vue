6256f63f04bd5293c923b090228d6345e0bc7bf1
Emmanuel-Dessaint <73949384+Emmanuel-Dessaint@users.noreply.github.com>
2021-08-21 17:41:25 +0200

    todolist

---
+++ resources/js/components/listView.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div v-for="(item, index) in items" :key="index">
+            <list-item
+            :item="item"           
+            v-on:reloadTodos="getTodos()"
+            class="item"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import listItem from './listItem'
+
+export default {
+    components: {
+        listItem
+        
+    },
+    data: function() {
+        return {
+            items: []
+        }
+    },
+    methods: {
+        getTodos() {
+            axios.get('api/todos').then(response => {
+                if(response.status === 200) {
+                    console.log(response.data.todos)
+                    this.items = response.data.todos
+                }
+            })
+        }
+    },
+    created() {
+        this.getTodos()
+    }
+}
+</script>
+
+<style>
+.item {
+    padding:12px;
+    margin-top:5px;
+    border-style:solid;
+    border-color:blue;
+    border-radius:7px;
+    border-width:1px;
+}
+</style>
